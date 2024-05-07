2dd4db2a3e95ae5a54d91ad837a4d41885210ace
v1per <107548344+v1peeer@users.noreply.github.com>
2024-05-07 21:30:52 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,25 +6,17 @@ CONFIG = json.load(
     open(os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), "config.json"))
 )
 
-
 def get_verbose():
-    # Read `config.json` file and get the value of `verbose` key
     return CONFIG["verbose"]
 
-
 def get_telegram_api_id():
-    # Read `config.json` file and get the value of `telegram_api_id` key
     return CONFIG["telegram_api_id"]
 
-
 def get_telegram_api_hash():
-    # Read `config.json` file and get the value of `telegram_api_hash` key
     return CONFIG["telegram_api_hash"]
 
 def get_phone():
-    # Get phone number from config
     return CONFIG["phone_number"]
 
 def get_db_file():
-    # Get the file name of the database from the config
     return CONFIG["db_file"]
